054253746c293b34859bcba9ed4fc3bc65bdca4e
Jawad Hussain <jawadduet@gmail.com>
2024-09-23 11:51:36 +0500

    Send data to client and parse it on client side

--- home/consumers.py
+++ home/consumers.py
@@ -3,6 +3,7 @@ from channels.consumer import SyncConsumer,AsyncConsumer
 from channels.exceptions import StopConsumer
 from time import sleep
 import asyncio
+import json
 
 
 class MySyncConsumer(SyncConsumer):
@@ -13,14 +14,23 @@ class MySyncConsumer(SyncConsumer):
             'type':'websocket.accept'
         })
 
+    # def websocket_receive(self,event):
+    #     print("Message recieved from client is ",event['text'])
+    #     for i in range(7):
+    #         self.send({
+    #             'type':'websocket.send',
+    #             'text':str(i)
+    #         })
+    #         sleep(1)
     def websocket_receive(self,event):
         print("Message recieved from client is ",event['text'])
         for i in range(7):
             self.send({
                 'type':'websocket.send',
-                'text':str(i)
+                'text':json.dumps({"count":i})
             })
             sleep(1)
+
 
     def websocket_disconnect(self,event):
         print("Websocket disconnected...")
